40bab542ae7e073ec8f169de331eb795005c7aaa
Nevervhoodo <dkhunter10@gmail.com>
2020-04-06 00:04:36 +0300

    Add method TargetOp::getArguments

--- php-chain/lib/TargetOp.php
+++ php-chain/lib/TargetOp.php
@@ -161,4 +161,53 @@ class TargetOp
         return $this->metric;
     }
 
+    public function getArguments()
+    {
+        $arguments = [];
+        // get all argument names
+        $type = $this->op->getType();
+        foreach ($this->op->getVariableNames() as $arg_name) {
+            $arg = $this->op->$arg_name;
+            if (is_null($arg)) {
+                continue;
+            }
+            switch ($arg_name) {
+                case 'result':
+                    break;
+                case 'name':
+                    if ( $type == 'Expr_FuncCall' or $type == 'Expr_MethodCall') {
+                        $arguments [] = $arg;
+                    }
+                    break;
+                case 'var':
+                    if ($type == 'Iterator_Value' or $type == 'Expr_ArrayDimFetch'
+                        or $type == 'Expr_PropertyFetch' or $type == 'Expr_ConstFetch') {
+                        $arguments [] = $arg;
+                    }
+                    break;
+                case 'list':
+                case 'args':
+                case 'vars':
+                case 'keys':
+                case 'values':
+                case 'cases':
+                case 'exprs':
+                case 'useVars':
+                    $arguments = array_merge($arguments, $arg);
+                    break;
+                case 'left':
+                case 'cond':
+                case 'right':
+                case 'expr':
+                case 'dim':
+                case 'key':
+                case 'class':
+                case 'nsName':
+                case 'defaultVar':
+                    $arguments [] = $arg;
+            }
+        }
+        return $arguments;
+    }
+
 }
